421404edd9a2ce5a6cd6a5fd93417de24755aaf7
하영 <82437499+summerlight636@users.noreply.github.com>
2022-08-29 19:00:42 +0900

    .

--- [인프런] 파이썬 알고리즘 문제풀이/1-9.주사위 게임.py
+++ [인프런] 파이썬 알고리즘 문제풀이/1-9.주사위 게임.py
@@ -1,12 +1,13 @@
-# 임포트
-
 # 입력
-map(int, input().split())
-
-# 상수
-
+n = int(input())
+for i in range(n):
+    a, b, c = map(int, input().split())
+    arr = set([a, b, c])
+    print(arr)
 # 알고리즘
+# 1. 같은 눈의 개수가 몇 개인지 판별 - "중복이니까" set 활용
 
+# 2. n개의 케이스 상금 계산 후 max 값
 
 # 출력
 print(result)
