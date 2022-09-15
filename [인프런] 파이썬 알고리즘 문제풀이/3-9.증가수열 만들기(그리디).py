98f34e024b59ad39587db064d2fbe5a0de171892
하영 <82437499+summerlight636@users.noreply.github.com>
2022-09-15 14:09:55 +0900

    #from collections import deque 어디서 임포트해야하는 지 알아둘 것 #deque는 앞 뒤 양쪽 방향에서 추가, 삭제가 엄청 빠르다. #왼쪽 끝 ~ 오른쪽 끝 이걸 보면 lt, rt 를 이용해서 포인터를 옮기는 방식을 생각해볼 수 있겠다. #마지막 하나 남은 건 왼쪽으로 보냐, 오른쪽으로 보냐는 어떤 if를 먼저 쓰느냐로 처리해줄 수 있다. #tmp = [] 로 하지말고, tmp = []는 맨 처음에만 선언하고 루프 마지막에서 tmp.clear() 를 이용해서 초기화 시켜주는 방법이 더 직관적이다.

--- [인프런] 파이썬 알고리즘 문제풀이/3-9.증가수열 만들기(그리디).py
+++ [인프런] 파이썬 알고리즘 문제풀이/3-9.증가수열 만들기(그리디).py
@@ -1,9 +1,36 @@
 #from collections import deque 어디서 임포트해야하는 지 알아둘 것
+#deque는 앞 뒤 양쪽 방향에서 추가, 삭제가 엄청 빠르다.
+#왼쪽 끝 ~ 오른쪽 끝 이걸 보면 lt, rt 를 이용해서 포인터를 옮기는 방식을 생각해볼 수 있겠다.
+#마지막 하나 남은 건 왼쪽으로 보냐, 오른쪽으로 보냐는 어떤 if를 먼저 쓰느냐로 처리해줄 수 있다.
+#tmp = [] 로 하지말고, tmp = []는 맨 처음에만 선언하고 루프 마지막에서 tmp.clear() 를 이용해서 초기화 시켜주는 방법이 더 직관적이다.
 from collections import deque
 n = int(input())
 lst = list(map(int, input().split()))
 deq = deque(lst)
 
+
+prev = 0
+res = ""
+temp = []
+while deq:
+    if prev < deq[0]:
+        temp.append((deq[0], 'L'))
+    if prev < deq[-1]:
+        temp.append((deq[-1], 'R'))
+    if len(temp) == 0:
+        break
+    else:
+        temp.sort()
+        res += temp[0][1]
+        if temp[0][1] == 'L':
+            prev = temp[0][0]
+            deq.popleft()
+        else:
+            deq.pop()
+    temp.clear()
+print(len(res))
+print(res)
+'''
 prev = 0
 while deq:
     lt = deq[0]
@@ -20,3 +47,4 @@ while deq:
             prev = lt
             deq.popleft()
             print('L', end='')
+'''
