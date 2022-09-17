9ea0733d83430ae785e5f2e4b6a26492e0ed9126
하영 <82437499+summerlight636@users.noreply.github.com>
2022-09-17 18:30:38 +0900

    # prev를 쓰지 않고 그냥 s[i], s[i-1] 을 이용할 수도 있다. # stack.pop() 이 중복되기 때문에 한곳으로 몰 수도 있다.

--- [인프런] 파이썬 알고리즘 문제풀이/4-2.쇠막대기(스택).py
+++ [인프런] 파이썬 알고리즘 문제풀이/4-2.쇠막대기(스택).py
@@ -1,9 +1,28 @@
+# prev를 쓰지 않고 그냥 s[i], s[i-1] 을 이용할 수도 있다.
+# stack.pop() 이 중복되기 때문에 한곳으로 몰 수도 있다.
+
 lst = list(map(str, input()))
 stack = []
 
 # 1. (는 바로 넣기
 # 2. )는 직전이 (일 경우 레이저: (하나 pop 하면서 res += len(stack)
 # 3. 아닐 경우는 막대 끝 (하나 pop 하면서 res += 1
+
+''' 모범 답안 
+s=input()
+stack=[]
+cnt=0
+for i in range(len(s)):
+    if s[i]=='(':
+        stack.append(s[i])
+    else:
+        stack.pop()
+        if s[i-1]=='(':
+            cnt+=len(stack)
+        else:
+            cnt+=1
+print(cnt)
+'''
 
 res = 0
 prev = ''
@@ -20,3 +39,4 @@ for x in lst:
     prev = x
 
 print(res)
+
