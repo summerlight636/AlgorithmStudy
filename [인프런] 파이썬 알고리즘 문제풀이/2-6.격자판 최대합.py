5f2455214da850f32bde8d5c100f8c169487243d
하영 <82437499+summerlight636@users.noreply.github.com>
2022-09-20 22:20:57 +0900

    폴더정리

--- [인프런] 파이썬 알고리즘 문제풀이/2-6.격자판 최대합.py
+++
@@ -1,8 +0,0 @@
-#2차원 배열 선언 a = [list(map(int, input().split())) for _ in range(n)]
-#행 탐색과 열 탐색은 순서만 바꾸면 된다.
-for i in range(n):
-    sum1 = 0
-    sum2 = 0 
-    for j in range(n):
-        sum1 += a[i][j] //i행의 합
-        sum2 += a[j][i] //j행의 합
