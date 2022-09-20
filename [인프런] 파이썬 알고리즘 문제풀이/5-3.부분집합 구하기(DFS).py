31a5971244ea7be87df42c98010834a8d4363907
하영 <82437499+summerlight636@users.noreply.github.com>
2022-09-20 22:25:27 +0900

    #상태트리: 왼쪽은 O 오른쪽은 X

---
+++ [인프런] 파이썬 알고리즘 문제풀이/5-3.부분집합 구하기(DFS).py
@@ -0,0 +1,16 @@
+#상태트리: 왼쪽은 O 오른쪽은 X
+def DFS(v):
+    if v==n+1:
+        for i in range(1, n+1):
+            if ch[i] == 1:
+                print(i, end=' ')
+        print()
+    else:
+        ch[v] = 1
+        DFS(v+1)
+        ch[v] = 0
+        DFS(v+1)
+
+n = int(input())
+ch = [0]*(n+1)
+DFS(1)
