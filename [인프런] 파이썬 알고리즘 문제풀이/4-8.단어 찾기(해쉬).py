55b0c0f63b3dbf9894a1079100db25fe311ce94e
하영 <82437499+summerlight636@users.noreply.github.com>
2022-09-20 22:23:19 +0900

    # 해쉬! # note = dict() # for key, val in p.items():

---
+++ [인프런] 파이썬 알고리즘 문제풀이/4-8.단어 찾기(해쉬).py
@@ -0,0 +1,13 @@
+# 해쉬!
+# note = dict()
+# for key, val in p.items():
+n = int(input())
+note = dict()
+for i in range(n):
+    note[input()] = 1
+for i in range(n-1):
+    note[input()] = 0
+for key, val in note.items():
+    if val == 1:
+        print(key)
+        break
