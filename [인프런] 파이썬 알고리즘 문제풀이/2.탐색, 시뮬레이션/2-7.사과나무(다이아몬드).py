c76a2fa44967bd60a15ce103f42eb5f58ddb71b5
하영 <82437499+summerlight636@users.noreply.github.com>
2022-09-20 22:50:17 +0900

    #다이아몬드 모양 어떻게 구현 할 지 바로 나오도록 #s, e 시작점, 끝점을 명시하면 직관적으로 표현할 수 있다. #j의 범위를 i와 n으로만 표현하려는 생각을 버리자.

---
+++ [인프런] 파이썬 알고리즘 문제풀이/2.탐색, 시뮬레이션/2-7.사과나무(다이아몬드).py
@@ -0,0 +1,21 @@
+#다이아몬드 모양 어떻게 구현 할 지 바로 나오도록
+#s, e 시작점, 끝점을 명시하면 직관적으로 표현할 수 있다.
+#j의 범위를 i와 n으로만 표현하려는 생각을 버리자.
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+total = 0
+
+s = e = n//2
+for i in range(n):
+    for j in range(s, e+1):
+        total += arr[i][j]
+    if i < n//2:
+        s -= 1
+        e += 1
+    else:
+        s += 1
+        e -= 1
+
+print(total)
