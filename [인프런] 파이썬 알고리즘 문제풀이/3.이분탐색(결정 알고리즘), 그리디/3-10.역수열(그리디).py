20fd155991819b1be98c89599b81ace5a8fa68e9
하영 <82437499+summerlight636@users.noreply.github.com>
2022-09-20 22:51:39 +0900

    #0의 개수 만큼 필요하다 => 처음 값에서 0을 만날 때마다 lst[i] -= 1 이렇게. #인덱스 값이랑 실제 숫자 값이랑 동일하게 하고 싶으면, lst.insert(0, 0)을 해주는 것이 편하다.

---
+++ [인프런] 파이썬 알고리즘 문제풀이/3.이분탐색(결정 알고리즘), 그리디/3-10.역수열(그리디).py
@@ -0,0 +1,17 @@
+#0의 개수 만큼 필요하다 => 처음 값에서 0을 만날 때마다 lst[i] -= 1 이렇게.
+#인덱스 값이랑 실제 숫자 값이랑 동일하게 하고 싶으면, lst.insert(0, 0)을 해주는 것이 편하다.
+n = int(input())
+lst = list(map(int, input().split()))
+
+res = [0]*n
+for i in range(n):
+    for j in range(n):
+        if lst[i] == 0 and res[j] == 0:
+            res[j] = i+1
+            break
+        elif res[j] == 0:
+            lst[i] -= 1
+
+for x in res:
+    print(x, end=' ')
+
