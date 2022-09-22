e3c5aac052d910feae5529ace0314760f276b4a7
하영 <82437499+summerlight636@users.noreply.github.com>
2022-09-23 00:00:42 +0900

    #import itertools as it #for tmp in it.combinations(a, k):

---
+++ [인프런] 파이썬 알고리즘 문제풀이/5-13.라이브러리를 이용한 조합.py
@@ -0,0 +1,14 @@
+#import itertools as it
+#for tmp in it.combinations(a, k):
+
+import sys
+import itertools as it
+sys.stdin=open("input.txt", "r")
+n, k=map(int, input().split())
+a=list(map(int, input().split()))
+m=int(input())
+cnt=0
+for x in it.combinations(a, k):
+    if sum(x)%m==0:
+        cnt+=1
+print(cnt)
