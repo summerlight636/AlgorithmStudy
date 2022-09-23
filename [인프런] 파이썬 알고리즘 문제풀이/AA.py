458a8b30a513a812d952de0bdf66a2c72a47e801
하영 <82437499+summerlight636@users.noreply.github.com>
2022-09-23 23:00:23 +0900

    .

--- [인프런] 파이썬 알고리즘 문제풀이/AA.py
+++ [인프런] 파이썬 알고리즘 문제풀이/AA.py
@@ -1,11 +1,26 @@
-#재귀함수로 탐색할 때 함수이름을 DFS로.
-#거꾸로 출력하려면 호출 밑으로 내려주면 거꾸로 출력된다. (스택을 사용하는 것이므로)
-#안했던 일을 하러 다시 온다는 의미에서 백트래킹이라고도 한다.
-n = int(input())
-def DFS(n):
-    if n==0:
-        return
+#문제를 이해를 못했다. 같은 무게의 추를 여러 번 담을 수 있는건가?
+#문제를 잘못 이해할 경우 잘못된 알고리즘을 작성하는데 드는 시간이 엄청나게 소모된다.
+#심지어 아예 불가능한 알고리즘에 대해 고민하게 되어 끝이 나지 않을 수도 있다.
+#그러니, 문제를 꼭 정확하게 이해하고, 세세한 조건들도 꼼꼼히 보자.
+#양팔 저울이므로 왼쪽 +1 오른쪽 -1 아무데도 안놓음 0 이렇게 처리할 수 있다.
+
+def DFS(l, sum):
+    if l == k:
+        if sum > 0:
+            res[sum] = 1
     else:
-        DFS(n//2)
-        print(n%2, end='')
-DFS(n)
+        DFS(l+1, sum+lst[l])
+        DFS(l+1, sum-lst[l])
+        DFS(l+1, sum)
+
+k = int(input()) #3<=k<=13
+lst = list(map(int, input().split())) #1~200000
+
+res = [0]*(sum(lst)+1)
+res[0] = 1
+DFS(0, 0)
+cnt = 0
+for i in range(sum(lst)+1):
+    if res[i] != 1:
+        cnt += 1
+print(cnt)
