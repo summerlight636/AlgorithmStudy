dca0bcc14b4acdf96bf24355a3dcc0c630690249
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-10-15 13:06:46 +0900

    #에라토스테네스 체: 2의 배수 모두 제외, 그 다음 3의 배수 모두 제외, ... #for 문에서 step을 i로 두고 검사

--- [인프런] 파이썬 알고리즘 문제풀이/1.구현/1-7.소수의 개수(에라토스테네스 체).py
+++ [인프런] 파이썬 알고리즘 문제풀이/1.구현/1-7.소수의 개수(에라토스테네스 체).py
@@ -1,11 +1,8 @@
-# 임포트
-
 # 입력
 n=int(input())
 
-# 상수
-
-# 알고리즘
+#에라토스테네스 체: 2의 배수 모두 제외, 그 다음 3의 배수 모두 제외, ...
+#for 문에서 step을 i로 두고 검사
 ch = [0]*(n+1)
 cnt = 0
 for i in range(2, n+1):
@@ -14,5 +11,4 @@ for i in range(2, n+1):
 		for j in range(i, n+1, i):
 			ch[j] = 1
 
-# 출력
 print(cnt)
