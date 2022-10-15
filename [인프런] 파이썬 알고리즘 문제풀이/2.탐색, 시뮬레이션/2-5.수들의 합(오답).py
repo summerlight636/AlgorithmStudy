6478bed222463457b644aaf9c4b7641df13ed94d
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-10-15 13:08:17 +0900

    #포인터 잡을때 lt, rt 와 같이 잡는다. (무조건 lt < rt 이기 때문) #p1, p2 와 같이 잡을 때에는 서로 대소관계가 유지되지 않는 경우에 쓴다.
    #시간초과 나온 코드: 이유 질문/ 나중에 확인하기!/ n^2 의 알고리즘이기 때문이다.

--- [인프런] 파이썬 알고리즘 문제풀이/2.탐색, 시뮬레이션/2-5.수들의 합(오답).py
+++ [인프런] 파이썬 알고리즘 문제풀이/2.탐색, 시뮬레이션/2-5.수들의 합(오답).py
@@ -1,7 +1,7 @@
 #포인터 잡을때 lt, rt 와 같이 잡는다. (무조건 lt < rt 이기 때문)
 #p1, p2 와 같이 잡을 때에는 서로 대소관계가 유지되지 않는 경우에 쓴다.
 
-#시간초과 나온 코드: 이유 질문/ 나중에 확인하기!
+#시간초과 나온 코드: 이유 질문/ 나중에 확인하기!/ n^2 의 알고리즘이기 때문이다.
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
 
@@ -17,3 +17,27 @@ for lt in range(n):
         rt += 1
 
 print(cnt)
+
+# 새로 짠 코드
+# 2-5.수들의 합
+#
+# n, m = map(int, input().split())
+# numbers = list(map(int, input().split()))
+#
+# sum = 0
+# cnt = 0
+# for i in range(n):
+#     sum = numbers[i]
+#     if sum == m:
+#         cnt += 1
+#         continue
+#
+#     for j in range(i+1, n):
+#         sum += numbers[j]
+#         if sum == m:
+#             cnt += 1
+#             break
+#         elif sum > m:
+#             break
+#
+# print(cnt)
